e2aaa5e168d4a193eb30262f250d5c0188d35d66
QuinnGiffel <quinn.giffel27@thalesacademy.org>
2024-09-06 17:24:38 -0400

    Quinn and ryan coded before i got here idk what they did I added beepboop also added in zegloub

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/beepboop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/beepboop.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "lights")
-public class lights extends OpMode {
+public class beepboop extends OpMode {
 
     Zegloub robot;
 
